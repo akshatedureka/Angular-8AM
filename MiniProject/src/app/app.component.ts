18ba8ee55c454123bbe62eb1551ae3a962f18554
Sambasivarao <samba.pendela@gmail.com>
2022-04-23 07:43:09 +0530

    miniproject

--- MiniProject/src/app/app.component.ts
+++ MiniProject/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { productsListAction } from './actions/products.actions';
+import Product from './model/products.model';
+import { productsSelector } from './selector/products.selector';
+import ProductsState from './state/products.state';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniProject';
+    title = 'MiniProject';
+    constructor(private store:Store<ProductsState>){}
+
+    loading!:boolean;
+    error!:string;
+    products!:Product[];
+
+    _subscription(){
+      this.store.select(productsSelector).subscribe((posRes)=>{
+          console.log(posRes);
+          const {loading,error,products} = posRes;
+          this.loading =   loading;
+          this.error = error;
+          this.products = products;
+      },(errRes)=>{
+        console.log(errRes);
+      });
+    }
+
+    _dispatch(){
+      this.store.dispatch(productsListAction());
+    }
+
+
+    ngOnInit(){
+        this._subscription();
+        this._dispatch();
+    }
+
+
+
+
 }
