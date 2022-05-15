24ecabdfb3a1c4451cedfe4ad1aba6406949b841
Sambasivarao <samba.pendela@gmail.com>
2022-05-15 09:54:05 +0530

    angular forms

---
+++ templateDrivenFormsEx1/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+      title = 'templateDrivenFormsEx1';
+
+
+      formData(data:any){
+          console.log(data);
+      }
+}
